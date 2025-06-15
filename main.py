45447cdb8a3fc836c29377e4289e9494780465f0
rinshan77 <simon.durakovic@proton.me>
2025-06-15 19:02:21 +0200

    Added shooting method

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import sys
 import pygame
 from constants import *
 from player import Player
-from asteroid import Asteroid
+from asteroid import Asteroid, Shot
 from asteroidfield import AsteroidField
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
 
 Player.containers = (updatable, drawable)
 Asteroid.containers = (asteroids, updatable, drawable)
 AsteroidField.containers = (updatable)
-
+Shot.containers = (shots, updatable, drawable)
 
 
 def main():
